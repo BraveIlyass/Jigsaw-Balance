a310701397fffe2593937bcd3497825dd1d297a0
Ilyas <il.cherkaoui@gmail.com>
2023-11-03 18:57:43 +0100

    Updated the scale mechanics
    Eliminated the bug where the scale keeps going down.
    i track the number of colliding objects.

--- Jigsaw Balance/Assets/WeightScale.cs
+++ Jigsaw Balance/Assets/WeightScale.cs
@@ -5,8 +5,7 @@ public class WeightScale : MonoBehaviour
     Rigidbody2D scaleRB;
     private Vector3 neutralPosition;
     private Vector3 targetPosition;
-    private bool weightWasAdded = false;
-    private bool weightWasRemoved = false;
+    private int numCollisions = 0;
     private float totalMass;
 
     void Start()
@@ -27,8 +26,7 @@ public class WeightScale : MonoBehaviour
         Rigidbody2D collidedRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
         if (collidedRigidbody != null)
         {
-            weightWasAdded = true;
-            weightWasRemoved = false;
+            numCollisions++;
             totalMass += collidedRigidbody.mass;
             targetPosition.y -= collidedRigidbody.mass;
         }
@@ -39,8 +37,7 @@ public class WeightScale : MonoBehaviour
         Rigidbody2D collidedRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
         if (collidedRigidbody != null)
         {
-            weightWasAdded = false;
-            weightWasRemoved = true;
+            numCollisions--;
             totalMass -= collidedRigidbody.mass;
             targetPosition.y += collidedRigidbody.mass;
         }
@@ -48,7 +45,7 @@ public class WeightScale : MonoBehaviour
 
     void ApplyWeightOnScale()
     {
-        if (weightWasAdded)
+        if (numCollisions > 0)
         {
             if (scaleRB.position.y <= targetPosition.y)
             {
@@ -63,9 +60,9 @@ public class WeightScale : MonoBehaviour
 
     void RemoveWeightFromScale()
     {
-        if (weightWasRemoved)
+        if (numCollisions == 0)
         {
-            if (scaleRB.position.y <= targetPosition.y)
+            if (scaleRB.position.y < targetPosition.y)
             {
                 scaleRB.bodyType = RigidbodyType2D.Dynamic;
                 scaleRB.gravityScale = -1;
